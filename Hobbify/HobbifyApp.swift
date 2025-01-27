3a677a11ead107058feaf020537f2d387304ce6c
maxowbricker <128575472+maxowbricker@users.noreply.github.com>
2025-01-27 13:19:45 +1100

    Initial Commit

---
+++ Hobbify/HobbifyApp.swift
@@ -0,0 +1,20 @@
+//
+//  HobbifyApp.swift
+//  Hobbify
+//
+//  Created by Max Bricker on 27/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct HobbifyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
